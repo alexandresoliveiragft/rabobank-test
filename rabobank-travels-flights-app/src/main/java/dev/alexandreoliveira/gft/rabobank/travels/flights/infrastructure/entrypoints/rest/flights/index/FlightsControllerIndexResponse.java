86679428bcb54b338a2b1daffb66c66460dc7888
Alexandre Salvador de Oliveira <alexandre.salvador.ext@dasa.com.br>
2024-03-04 14:33:21 -0300

    [feat] Add attributes in flights index response
    Add flightNumber and price in response index flights.

--- rabobank-travels-flights-app/src/main/java/dev/alexandreoliveira/gft/rabobank/travels/flights/infrastructure/entrypoints/rest/flights/index/FlightsControllerIndexResponse.java
+++ rabobank-travels-flights-app/src/main/java/dev/alexandreoliveira/gft/rabobank/travels/flights/infrastructure/entrypoints/rest/flights/index/FlightsControllerIndexResponse.java
@@ -2,6 +2,7 @@ package dev.alexandreoliveira.gft.rabobank.travels.flights.infrastructure.entryp
 
 import dev.alexandreoliveira.gft.rabobank.travels.flights.core.models.FlightModel;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -13,22 +14,26 @@ public record FlightsControllerIndexResponse(
     public record Flight(
             UUID id,
             String company,
+            Integer flightNumber,
             String origin,
             String destiny,
             List<Seat> availableSeats,
             LocalDateTime checkIn,
-            LocalDateTime checkOut
+            LocalDateTime checkOut,
+            BigDecimal price
     ) {
 
         public Flight(FlightModel model) {
             this(
                     model.getId(),
                     model.getCompany(),
+                    model.getFlightNumber(),
                     model.getOrigin(),
                     model.getDestiny(),
                     model.getSeats().stream().map(seatModel -> new Seat(seatModel.getId(), seatModel.getSeatNumber())).toList(),
                     model.getCheckIn(),
-                    model.getCheckOut());
+                    model.getCheckOut(),
+                    model.getPrice());
         }
     }
 
