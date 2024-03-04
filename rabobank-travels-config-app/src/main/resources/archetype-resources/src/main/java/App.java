8f527f7da232e90aeedd36a89f01373b7e496fe4
Alexandre Salvador de Oliveira <alexandre.salvador.ext@dasa.com.br>
2024-03-04 16:45:31 -0300

    [feat] Add config server
    Remove database dependencies from principal pom
    
    Add database dependencies in projects
    - travels-app
    - travels-flights-app
    - travels-hotels-app
    - travels-rent-car-app
    
    Create config server module

---
+++ rabobank-travels-config-app/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $dev.alexandreoliveira.gft.rabobank.travels.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
